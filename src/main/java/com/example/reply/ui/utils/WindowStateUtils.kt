33f1bca21662fd39519879b43b9e09bf72070cde
ImYichus <s22120205@alumnos.itsur.edu.mx>
2025-10-21 20:57:00 -0600

    VERSION 4.0

--- src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
+++ src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -19,5 +19,8 @@ package com.example.reply.ui.utils
  * Different type of navigation supported by app depending on size and state.
  */
 enum class ReplyNavigationType {
-    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER,LIST_ONLY, LIST_AND_DETAIL
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
+enum class ReplyContentType {
+    LIST_ONLY, LIST_AND_DETAIL
 }
